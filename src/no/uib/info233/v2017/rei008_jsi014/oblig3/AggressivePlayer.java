b4db023f2c2acface320fc3e332f5cd0313978a4
Rune Eikemo <rune.eikem@gmail.com>
2017-04-21 12:58:12 +0200

    + fiksa litt på makenextmove

--- src/no/uib/info233/v2017/rei008_jsi014/oblig3/AggressivePlayer.java
+++ src/no/uib/info233/v2017/rei008_jsi014/oblig3/AggressivePlayer.java
@@ -1,8 +1,5 @@
 package no.uib.info233.v2017.rei008_jsi014.oblig3;
 
-
-import java.util.Random;
-
 public class AggressivePlayer extends Player {
 
     public AggressivePlayer(String name) {
@@ -11,44 +8,56 @@ public class AggressivePlayer extends Player {
 
     @Override
     public void makeNextMove(int currentPosition, int yourEnergy, int opponentEnergy) {
-        System.out.println(yourEnergy);
-        int randMove = rand.nextInt(4);
-        int useEnergy;
-        switch (currentPosition) {
-            case 0:
-            case 1: //25% whirlwind(), 75% slash() for position 0 and 1
-                if (randMove == 0) {
-                    useEnergy = whirlwind(yourEnergy);
-                } else
-                    useEnergy = slash(yourEnergy);
-                break;
-            case 2:
-                useEnergy = overheadSwing(yourEnergy);
-                break;
-            case 3:
-            case 4: // 50% chance of using stab(),50% of using overheadSwing in position 3 and 4
-                if (randMove == 0 || randMove == 1) {
-                    useEnergy = stab(yourEnergy);
-                } else {
-                    useEnergy = overheadSwing(yourEnergy);
-                }
-                break;
-            case 5:
-            case 6: // 25% of using slash(), 25% stab(), 50% whirilwind()
-                if (randMove == 0) {
-                    useEnergy = slash(yourEnergy);
-                } else if (randMove == 1) {
-                    useEnergy = stab(yourEnergy);
-                } else
-                    useEnergy = whirlwind(yourEnergy);
-                break;
-            default:
-                useEnergy = 0;
-                break;
-
+        if(yourEnergy < 0) {
+            yourEnergy = 0;
+            setCurrentEnergy(yourEnergy);
         }
-        this.updateEnergy(-useEnergy);
-        this.getGameMaster().listenToPlayerMove(this, useEnergy);
+        int randMove = rand.nextInt(4);
+        int useEnergy = 0;
+        if (yourEnergy > 0) {
+
+            // If yourEnergy is bigger than the actual energy set yourEnergy to actual energy
+            if (yourEnergy > this.getCurrentEnergy()) {
+                yourEnergy = getCurrentEnergy();
+            }
+
+            // Detects currentPosition and assigns the energy to use
+            switch (currentPosition) {
+                case 0:
+                case 1: //25% whirlwind(), 75% slash() for position 0 and 1
+                    if (randMove == 0) {
+                        useEnergy = whirlwind(yourEnergy);
+                    } else
+                        useEnergy = slash(yourEnergy);
+                    break;
+                case 2:
+                    useEnergy = overheadSwing(yourEnergy);
+                    break;
+                case 3:
+                case 4: // 50% chance of using stab(),50% of using overheadSwing in position 3 and 4
+                    if (randMove == 0 || randMove == 1) {
+                        useEnergy = stab(yourEnergy);
+                    } else {
+                        useEnergy = overheadSwing(yourEnergy);
+                    }
+                    break;
+                case 5:
+                case 6: // 25% of using slash(), 25% stab(), 50% whirilwind()
+                    if (randMove == 0) {
+                        useEnergy = slash(yourEnergy);
+                    } else if (randMove == 1) {
+                        useEnergy = stab(yourEnergy);
+                    } else
+                        useEnergy = whirlwind(yourEnergy);
+                    break;
+                default:
+                    useEnergy = 0;
+                    break;
+
+            }
+            this.updateEnergy(-useEnergy);
+            this.getGameMaster().listenToPlayerMove(this, useEnergy);
+        }
     }
 
     /**
